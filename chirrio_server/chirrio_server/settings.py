6f4326b7b1ac7e8f3d91ab9e95e54150c257f2ae
gleb-skobinsky <glebgytnik@gmail.com>
2023-09-10 03:09:48 +0300

    Static files

--- chirrio_server/chirrio_server/settings.py
+++ chirrio_server/chirrio_server/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from django.core.management.commands.runserver import Command
 
-Command.default_port = '8080'        # <-- Your port
-Command.default_addr = '127.0.0.1'   # <-- Your address
+Command.default_port = "8080"
+Command.default_addr = "127.0.0.1"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fjp1iciu&wq+m0f+yzhpnieuu&ai##juzb)fl#esrc2eobnlx2"
+SECRET_KEY = "g-jx@69k2b6fge5=lj%(hfisa&3i!qhsgw!no3vnpr2i$of$lv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
