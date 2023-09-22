50788208ed121e15a49b0b139fbf69d4ad87ece7
gleb-skobinsky <glebgytnik@gmail.com>
2023-09-22 09:36:07 +0300

    Prepare for redeployment

--- chirrio_server/chirrio_server/settings.py
+++ chirrio_server/chirrio_server/settings.py
@@ -153,7 +153,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1" if DEBUG else "redis", 6379)],
+            "hosts": [("redis", 6379)],
         },
     },
 }
