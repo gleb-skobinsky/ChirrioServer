ecc1ee01dd46f9e3712ad1c1533ea58676b6d8dc
gleb-skobinsky <glebgytnik@gmail.com>
2023-09-22 09:52:51 +0300

    Fix daphne app

--- chirrio_server/chirrio_server/asgi.py
+++ chirrio_server/chirrio_server/asgi.py
@@ -6,21 +6,21 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
 
-from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chirrio_server.settings")
+django_asgi_app = get_asgi_application()
 
 from chat import routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chirrio_server.settings")
-
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
         ),
