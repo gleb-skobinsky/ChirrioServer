3ab216ab5591e72b3360661d04de6dc47848aa14
gleb-skobinsky <glebgytnik@gmail.com>
2023-10-18 00:54:50 +0300

    Db to postgres

--- chirrio_server/chirrio_server/settings.py
+++ chirrio_server/chirrio_server/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = "chirrio_server.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -113,7 +113,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
